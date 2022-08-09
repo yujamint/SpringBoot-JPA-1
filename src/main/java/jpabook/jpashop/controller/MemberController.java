dff6021c6c7291e6a10ca4366a877eba91c1beca
yujaemin <woawoa0513@gmail.com>
2022-08-09 16:30:57 +0900

    기능추가: 멤버 컨트롤러 추가

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -31,7 +32,7 @@ public class MemberController {
     @PostMapping("/members/new")
     public String create(@Valid MemberForm form, BindingResult result) {
 
-        if (result.hasErrors()){
+        if (result.hasErrors()) {
             return "members/createMemberForm";
         }
 
@@ -43,5 +44,12 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
